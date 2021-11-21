c38286dd0b2086c8f1bfbacba206d88fdfe090df
Hanzala Sohrab <sorb.hanzala@gmail.com>
2021-11-21 23:03:32 +0530

    Update timezone (IST)

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from datetime import datetime
+import pytz
 
+from datetime import datetime
 from flask import request, Flask
 
 app = Flask(__name__)
 
+IST = pytz.timezone('Asia/Kolkata')
+
 @app.route("/get-hour")
 def get_hour():
-    return {"hour": datetime.now().hour}
+    return {"hour": datetime.now(IST).hour}
 
 if __name__ == '__main__':
     app.run()
